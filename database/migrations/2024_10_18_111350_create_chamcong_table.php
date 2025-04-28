d87fcb7edca73d51d06a5dacd9d67aebee3333c6
quan27102002 <quannedungso@gmail.com>
2025-04-28 12:46:44 +0700

    first commit

---
+++ database/migrations/2024_10_18_111350_create_chamcong_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chamcong', function (Blueprint $table) {
+            $table->id('maChamCong');
+            $table->time('gioCheckin');
+            $table->time('gioCheckout')->nullable();
+            $table->date('ngay');
+            $table->integer('tongGioLam')->nullable();
+            $table->float('cong', 3, 2)->nullable();
+            $table->foreignId('maND')->constrained('nguoidung');
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chamcong');
+    }
+};
---
+++ database/migrations/2024_10_18_111407_create_vitri_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vitri', function (Blueprint $table) {
+            $table->id('maViTri');
+            $table->string('kinhDo');
+            $table->string('viDo');
+            $table->string('trangThai');
+            $table->foreignId('maCongTy')->constrained('congty')->onDelete('cascade'); 
+            $table->foreignId('maND')->constrained('nguoidung')->onDelete('cascade'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vitri');
+    }
+};
